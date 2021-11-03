3e01ea4ed59791468ce77c8cff13dd1ba7cfb990
Linuxpert <g.manfre10@gmail.com>
2021-11-03 14:30:39 +0100

    primo tentativo

---
+++ js/script.js
@@ -0,0 +1,109 @@
+//1.Chiedere all'utente il livello di difficoltà del gioco => promt
+//2.generare una griglia di gioco quadrata in base alla difficoltà => if
+    //2.1 contentente delle celle => ciclo for
+//3.quando l'utente clicca il quadrato cambia colore => click
+
+const grid = document.getElementById("grid");
+
+//chiedere all'utente il livello di difficoltà da 1 a 3
+let difficultGame = parseInt(prompt("Inserisci un numero da 1 a 3 per la difficoltà, più il numero è alto più sale la difficoltà."));
+
+//controllo della difficoltà, se l'utente non inserisce un numero tra 1 e 3 il promt compare di nuovo
+while((difficultGame < 1) || (difficultGame > 3)) {
+    difficultGame = parseInt(prompt("Inserisci un numero da 1 a 3 per la difficoltà, più il numero è alto più sale la difficoltà."));
+}
+
+
+
+const bomb = [];
+
+
+
+//in base alla difficoltà si generano tot quadrati
+if (difficultGame === 1) {
+    for(let i = 1; i <= 100; i++){
+        // console.log(i);
+        let newElem = createSquare ("div", "square1");
+
+        
+        while(bomb.length < 16){
+            const num = Math.floor(Math.random() * 100) + 1;
+            let sameNum = bomb.includes(num);
+        
+            if(sameNum === false){
+                bomb.push(num)
+            }
+        }
+        // console.log(bomb);
+
+
+        if(i === bomb[i]){
+            console.log("sono uguale");
+            newElem.addEventListener("click",
+                function(){
+                this.classList.add("clicked-false");
+                newElem.innerText = `${i}`
+            }
+            );
+
+        } else if(i !== bomb[i]){
+            console.log("non sono uguale");
+            newElem.addEventListener("click",
+                function(){
+                this.classList.add("clicked-true");
+                newElem.innerText = `${i}`
+            }
+            );
+        }
+
+        
+
+        grid.appendChild(newElem);
+
+    }
+}else if(difficultGame === 2){
+
+    for(let i = 1; i <= 81; i++){
+
+        let newElem = createSquare ("div", "square2");
+        
+        newElem.addEventListener("click",
+            function(){
+                this.classList.add("clicked-true");
+                newElem.innerText = `${i}`
+            }
+        );
+
+        grid.appendChild(newElem);
+
+    }
+
+}else if(difficultGame === 3){
+
+    for(let i = 1; i <= 49; i++){
+
+        let newElem = createSquare ("div", "square3");
+
+        newElem.addEventListener("click",
+            function(){
+                this.classList.add("clicked-true");
+                newElem.innerText = `${i}`
+            }
+        );
+
+        grid.appendChild(newElem);
+
+    }
+}
+
+
+//funzioni
+
+function createSquare (squareGen, classPlus) {
+    let nodeHundred = document.createElement(squareGen);
+    nodeHundred.classList.add(classPlus);
+
+    return nodeHundred;
+}
+
+
